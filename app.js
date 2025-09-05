f4177ce176ef1880aa5f976b15d66857d0c0f1d0
Alexander Novitsky <novitsky.413x@gmail.com>
2025-09-05 12:22:15 +0300

    315. Using the Error Handling Middleware Correctly

--- app.js
+++ app.js
@@ -38,11 +38,18 @@ app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, 
 app.use(csrfProtection);
 app.use(flash());
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+app.use((req, res, next) => {
+    // throw new Error('Sync Dummy error');
     if (!req.session.user) {
         return next();
     }
     User.findById(req.session.user._id)
         .then((user) => {
+            // throw new Error('Async Dummy error');
             if (!user) {
                 return next();
             }
@@ -50,21 +57,24 @@ app.use((req, res, next) => {
             next();
         })
         .catch((err) => {
-            throw new Error(err);
+            next(new Error(err));
+            // inside async code, we need to call next() with an argument
+            // to pass the error to the error handling middleware
         });
 });
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
-});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 app.get('/500', errorController.get500);
 app.use(errorController.get404);
 app.use((error, req, res, next) => {
-    res.redirect('/500');
+    // res.redirect('/500');
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn,
+    });
 });
 
 mongoose
